bfe865fd7064639dc9b57234d57bf9fdf09b156e
Alan Witkowski <alan.witkowski@gmail.com>
2014-01-10 18:04:50 -0700

    updated music dir

--- move.sh
+++ move.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # move files over to music directory
-find . -mindepth 1 -maxdepth 1 -type d -and -not -path "./.*" -exec cp -r {} /media/backup/Music/FLAC/ \; -exec rm -rf {} \;
+find . -mindepth 1 -maxdepth 1 -type d -and -not -path "./.*" -exec cp -r {} /mnt/backup/Music/FLAC/ \; -exec rm -rf {} \;
 
 rm -f *.wav
