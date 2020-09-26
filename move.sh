4f7ee61b375b9ce3ca00267f94c7d38d83a68c66
Alan Witkowski <alan.witkowski@gmail.com>
2020-09-26 10:32:52 -0600

    path fix

--- move.sh
+++ move.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # move files over to music directory
-find . -mindepth 1 -maxdepth 1 -type d -and -not -path "./.*" -exec scp -r {} main:/mnt/backup/music/flac/ \; -exec rm -rf {} \;
+find . -mindepth 1 -maxdepth 1 -type d -and -not -path "./.*" -exec scp -r {} main:/mnt/storage/music/flac/ \; -exec rm -rf {} \;
 
 rm -f *.wav
